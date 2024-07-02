e8d3431b54b411d735ea5f2581970969ba9c8a7c
veyhn <vennisabarfi@gmail.com>
2024-07-01 20:16:14 -0400

    Go set up and server running

--- backend/go/main.go
+++ backend/go/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/joho/godotenv" //import godotenv
+
+	"os"
+)
+
+// load environment variables
+func env_Variable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+		port := env_Variable("PORT")
+		//check for errors
+		error := r.Run(port)
+
+		if error != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to retrieve tasks.",
+			})
+			return
+		}
+	})
 
 }

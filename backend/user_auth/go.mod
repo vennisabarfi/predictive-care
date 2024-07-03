87be54f201ce3d6e08eef1d21569ba8469559f39
veyhn <vennisabarfi@gmail.com>
2024-07-03 17:24:06 -0400

    set cookies and token secret key

--- backend/user_auth/go.mod
+++ backend/user_auth/go.mod
@@ -3,7 +3,12 @@ module user_auth
 go 1.22.4
 
 require (
+	github.com/gin-contrib/sessions v1.0.1
+	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.24.0
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.10
 )
@@ -14,9 +19,7 @@ require (
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/sessions v1.0.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
@@ -32,16 +35,16 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
--- backend/user_auth/main.go
+++ backend/user_auth/main.go
@@ -5,46 +5,47 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 	"user_auth/models"
 	"user_auth/storage"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
-const userkey = "user"
+// const userkey = "user"
 
-var secret = []byte("secret")
+// var secret = []byte("secret")
 
-// Cookie and Session Management
-func engine() *gin.Engine {
-	r := gin.New()
+// // Cookie and Session Management
+// func engine() *gin.Engine {
+// 	r := gin.New()
 
-	//Setup cookie store for session management
-	r.Use(sessions.Sessions("mysession", cookie.NewStore(secret)))
+// 	//Setup cookie store for session management
+// 	r.Use(sessions.Sessions("mysession", cookie.NewStore(secret)))
 
-	//Login and logout routes
-	r.POST("/login", login)
-	r.GET("/logout", logout)
+// 	//Login and logout routes
+// 	r.POST("/login", login)
+// 	r.GET("/logout", logout)
 
-	return r
-}
+// 	return r
+// }
 
-// Middleware to check the session
-func AuthRequired(c *gin.Context) {
-	session := sessions.Default(c)
-	user := session.Get(userkey)
-	if user == nil {
-		//Abort the request with the appropriate error code
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unathorized"})
-		return
-	}
-	// Continue down the chain to handler etc
-	c.Next()
-}
+// // Middleware to check the session
+// func AuthRequired(c *gin.Context) {
+// 	session := sessions.Default(c)
+// 	user := session.Get(userkey)
+// 	if user == nil {
+// 		//Abort the request with the appropriate error code
+// 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unathorized"})
+// 		return
+// 	}
+// 	// Continue down the chain to handler etc
+// 	c.Next()
+// }
 
 // login handler. Parses a form and checks for specific data
 func login(c *gin.Context) {
@@ -72,8 +73,40 @@ func login(c *gin.Context) {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"message": "Invalid email or password",
 			})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Server error",
+			})
 		}
+		return
 	}
+	//Compare password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid email or password",
+		})
+
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed to create token",
+		})
+		return
+	}
+
+	//Set Cookie
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", true, true)
 }
 
 func logout(c *gin.Context) {
